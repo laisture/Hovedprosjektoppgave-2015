2377a328182fcb093bb8cd35f2b7c5208c060d30
Joakim <Joakim@10.253.1.90>
2015-04-15 13:03:50 +0200

    Version 1, Joakim

---
+++ src/Skademelding.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Joakim
+ */
+public class Skademelding {
+    
+    private Kunde kunde;
+    private String melding;
+    private String dato;
+    private String type;
+    //<Bildeklasse>
+    private String vitne;
+    private int takst;
+    
+    public Skademelding(Kunde k, String s)
+    {
+        
+    }
+    
+    public Boolean harForsikring(String t)
+    {
+        //<Metode for å sjekke om kunden har riktig forsikring>
+    }
+    
+    
+}
