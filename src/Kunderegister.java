3c7427d7fad0508f10ec681ad5d73d28323e35fc
Joakim <Joakim@10.253.1.90>
2015-04-15 12:38:18 +0200

    Version 1, Joakim

---
+++ src/Kunderegister.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Joakim
+ */
+public class Kunderegister {
+    
+   LinkedList register =new LinkedList();
+    
+    public Kunderegister()
+    {
+        
+    }
+    // <Metorder for: Oppretting av kunder og forsikringer. >
+    //<Metoder for søke gjennom kunder og forsikringer>
+    //<Metode for å langre>
+    
+}
