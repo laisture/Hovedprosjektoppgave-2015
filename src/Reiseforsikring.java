68908606c21b252e284d30d7b79393e43d6879ea
Joakim <Joakim@10.253.3.246>
2015-04-17 12:57:16 +0200

    Version 2, Joakim

--- src/Reiseforsikring.java
+++ src/Reiseforsikring.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /**
  *
@@ -20,5 +15,14 @@ public class Reiseforsikring extends Forsikring
         område=o;
     }
     
-    
+    public String toString()
+    {
+        String melding=super.toString();
+        melding +="område for reiseforsikring: "+område+"\n";
+        if(sum>0)
+        {
+            melding+="sum utbetalt: "+sum+"\n";
+        }
+        return melding;
+    }
 }
