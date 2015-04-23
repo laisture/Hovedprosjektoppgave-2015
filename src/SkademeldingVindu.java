5d085d022b9ef19dfc84d101c682bef8fbf231ea
Joakim <Joakim@10.253.9.117>
2015-04-23 13:28:37 +0200

    Version 3, Joakim

--- src/SkademeldingVindu.java
+++ src/SkademeldingVindu.java
@@ -12,21 +12,41 @@ import java.awt.*;
  *
  * @author Joakim
  */
+
+//dette er en test
+//dette er en annen test
+
 public class SkademeldingVindu  extends JFrame  {
     
-    private JFrame vindu =new JFrame ("Frame");
-    private JPanel p= new JPanel(new GridLayout());
-    private JLabel l=new JLabel("Kunde");
+    private JFrame vindu =new JFrame ("Send inn ");
+    private JPanel p= new JPanel();
+    private JPanel s=new JPanel();
+    private JPanel b=new JPanel();
+    private JLabel k=new JLabel("Kunde");
+    private JLabel m=new JLabel("Skriv en beskrivelse av hendelsen samt skaden/skadene: ");
     private JButton knapp= new JButton("trykk her!");
     private JTextField kunde=new JTextField(10);
-    
+    private JTextArea beskrivelse= new JTextArea(1, 1);
+    JPanel container = new JPanel();
+   
+
     
     public SkademeldingVindu ()
     {
         
-        p.add(l);
+        p.setBorder( BorderFactory.createEmptyBorder( 10, 10, 10, 10 ) );
+
+    // Create the layout
+    GroupLayout layout = new GroupLayout( p );
+    p.setLayout( layout );
+    layout.setAutoCreateGaps( true );
+        
+        container.setLayout(new BoxLayout(container, BoxLayout.X_AXIS));
+        p.add(k);
         p.add(kunde);
-        p.add(knapp);
+        s.add(m);
+        s.add(beskrivelse);
+        b.add(knapp);
        
         /*Legger til ikon til vinduet
         String bildefil = "Ikon.png";
@@ -40,6 +60,31 @@ public class SkademeldingVindu  extends JFrame  {
         }*/
         vindu.add(p);
         vindu.setSize(400,400);
+        
+        layout.setHorizontalGroup( layout.createSequentialGroup()
+                                       .addGroup( layout.createParallelGroup( GroupLayout.Alignment.LEADING )
+                                                          .addComponent( k )
+                                                          
+                                       )
+                                       .addGroup( layout.createParallelGroup( GroupLayout.Alignment.LEADING )
+                                                          .addComponent( kunde )
+                                                          
+                                                           )
+    );
+         layout.setVerticalGroup( layout.createSequentialGroup()
+                                     .addGroup( layout.createParallelGroup( GroupLayout.Alignment.BASELINE )
+                                                        .addComponent( k )
+                                                        .addComponent( kunde ) )
+                                     
+                                    
+    );
+        
+        
+        vindu.add(container);
+        container.add(p);
+        container.add(b);
+        container.add(s);
+        vindu.setSize(300,400);
         vindu.setVisible(true);
         vindu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
