4509bf85aea62e55a2ecdd3ed89574b75c9e6110
Joakim <Joakim@10.253.0.212>
2015-05-14 14:33:56 +0200

    Version 10,Joakim

--- src/Bilforsikring.java
+++ src/Bilforsikring.java
@@ -62,59 +62,59 @@ public class Bilforsikring extends Forsikring {
     }
     // Metoden har som oppgave å regne ut bonus hos forsikringskunden. Maks bonus er 75 prosentpoeng.
     // Comment: Må kunne lagre opparbeidet bonus en plass?
-    public void bonus(Kunde k)
-    {   
-        Kunde kunde=k;
-        int dager =  24* 3600 * 1000;
-        long år = 365; 
-        long opprettet = Skademelding.getOpprettetlang()/dager;
-        long dagensDato = (long) (new Date().getTime())/dager;
-        int teller = 0;
-        Skademelding[] s=k.getSkademeldinger();
-        Skademelding siste=null;
-        
-        for (int i=0; i<s.length;i++)
-        {
-            if(s[i].getType()=="bil")
-            {
-                if (siste==null || opprettet>siste)
-                {
-                  siste=s[i];
-                }
-            }
-        }
-        
-        if (opprettet == dagensDato )
-        {
-            bonus+=20;
-        }
-        
-        long forskjell = dagensDato-opprettet;
-        while(forskjell >= 0)
-        {
-            forskjell -= år;
-            if(forskjell >= 0) // bonus skal øke med 10 for hvert år personen har hatt forsikringen
-            {
-                teller++;
-                bonus+=10;
-            }
-        }
-        if (teller >= 5 && bonus==70) // Øker bonusen hvis antall år 
-        {
-            bonus=75;
-        }
-        else
-        {
-           bonus=70;
-        }
-        if(s.length>0 && bonus==75 && teller<6) 
-        {
-            bonus=60;
-        }
-        else if(bonus<75 & s.length>0)
-        {
-            bonus-=30;
-        }
-        
-    }
+//    public void bonus(Kunde k)
+//    {   
+//        Kunde kunde=k;
+//        int dager =  24* 3600 * 1000;
+//        long år = 365; 
+//        long opprettet = Skademelding.getOpprettetlang()/dager;
+//        long dagensDato = (long) (new Date().getTime())/dager;
+//        int teller = 0;
+//        Skademelding[] s=k.getSkademeldinger();
+//        Skademelding siste=null;
+//        
+//        for (int i=0; i<s.length;i++)
+//        {
+//            if(s[i].getType()=="bil")
+//            {
+//                if (siste==null || opprettet>siste)
+//                {
+//                  siste=s[i];
+//                }
+//            }
+//        }
+//        
+//        if (opprettet == dagensDato )
+//        {
+//            bonus+=20;
+//        }
+//        
+//        long forskjell = dagensDato-opprettet;
+//        while(forskjell >= 0)
+//        {
+//            forskjell -= år;
+//            if(forskjell >= 0) // bonus skal øke med 10 for hvert år personen har hatt forsikringen
+//            {
+//                teller++;
+//                bonus+=10;
+//            }
+//        }
+//        if (teller >= 5 && bonus==70) // Øker bonusen hvis antall år 
+//        {
+//            bonus=75;
+//        }
+//        else
+//        {
+//           bonus=70;
+//        }
+//        if(s.length>0 && bonus==75 && teller<6) 
+//        {
+//            bonus=60;
+//        }
+//        else if(bonus<75 & s.length>0)
+//        {
+//            bonus-=30;
+//        }
+//        
+//    }
 }
--- src/test.java
+++ src/test.java
@@ -18,8 +18,7 @@ public class test {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        Kunderegister register= new Kunderegister();
+        
         
        
 			
